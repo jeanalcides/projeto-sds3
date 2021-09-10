a2bf19d526a7c5cf8d7ee6777ce1745359e4f0d5
jeanalcides <jean.ap44@gmail.com>
2021-09-10 13:14:53 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,20 +24,11 @@ const DonutCharts = () => {
             });
     }, []);
 
-
-
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
-
     const options = {
         legend: {
             show: true
         }
     }
-
 
     return (
         <Chart
